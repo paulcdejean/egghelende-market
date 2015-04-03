14f4247b12459946baaff53f475773581cc5fc86
Paul Dejean <paulcdejean@gmail.com>
2015-04-03 04:59:53 -0400

    Added a shopping list view.

--- market/urls.py
+++ market/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, url
 
 from market import views
 
-urlpatterns = patterns('',
-    url(r'^$', views.items, name='items'),
-)
+urlpatterns = patterns(
+    "",
+    url(r"^items$", views.items, name="items"),
+    url(r"^shopping$", views.shopping, name="shopping")
+    )
--- market/views.py
+++ market/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 from market.models import Item
+from django.http import HttpResponse
+
+from market.models import Item
+from market.models import Material
+from market.models import ItemMaterial
+
 
 def items(request):
     return render(request, "items.html", {"items": Item.objects.all()})
+
+def shopping(request):
+    # Hard coding this because I'm lazy.
+    spacing = 15
+
+    result = ""
+    for material in Material.objects.all():
+        total = 0
+        for item_material in ItemMaterial.objects.filter(material=material):
+            total += item_material.quantity * item_material.item.in_need
+        result += str(total) + " " * (spacing - len(str(total)))
+        result += material.material_name + "\n"
+    return HttpResponse(result, content_type="text/plain")
